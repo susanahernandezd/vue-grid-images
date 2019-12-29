fff2204943ca7eb589743546fa0cc69cd6b0dbea
Susana <susanahernandezey@gmail.com>
2019-12-29 17:53:22 +0100

    Add App and Card vue component and create scss files loaded from vue app

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-row>
+    <b-col cols="4">
+      <div class="card">
+        <b-img class="card__image img-responsive" fluid src="https://via.placeholder.com/600/92c952" alt=""></b-img>
+        <div class="card__text">
+          <h2 class="card__title">accusamus beatae ad facilis cum similique qui sunt</h2>
+          <button class="card__button">
+            <span>Remove image</span>
+          </button>
+        </div>
+      </div>
+    </b-col>
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
+
+<style lang="scss">
+@import './src/styles/components/_card';
+</style>
---
+++ src/infrastructure/Configuration.js
@@ -0,0 +1,3 @@
+export const {
+  VUE_APP_CONTENT_API: contentApi
+} = process.env
