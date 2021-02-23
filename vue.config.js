611bfeaed2b7b468e77c52ff48cc7d8b0a8f1a1b
Susana Hernandez <shernandez@plainconcepts.com>
2021-02-23 11:04:12 +0100

    Default vue config pwa

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports = {
   chainWebpack: config => {
     config.plugins.delete('preload')
     config.plugins.delete('prefetch')
+    config.plugin('workbox')
     config.resolve.symlinks(false)
   },
   configureWebpack: {
@@ -17,6 +18,15 @@ module.exports = {
     'style-resources-loader': {
       preProcessor: 'scss',
       patterns: [path.resolve(__dirname, './src/styles/main.scss')]
+    }
+  },
+  pwa: {
+    workboxPluginMode: 'GenerateSW',
+    themeColor: '#42b983',
+    msTileColor: '#42b983',
+    appleMobileWebAppCache: 'yes',
+    manifestOptions: {
+      background_color: '#42b983'
     }
   }
 
